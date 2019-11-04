e90bf372fe4fcf19f7c91f808cae3de8f46a68b0
Michael Bengtsson <michael@krokedil.se>
2019-11-04 17:06:19 +0100

    Version 2.0.1 fix

--- classes/requests/class-kp-requests.php
+++ classes/requests/class-kp-requests.php
@@ -43,7 +43,7 @@ class KP_Requests {
 	public function set_environment_variables() {
 		// Set variables.
 		$this->kp_settings = get_option( 'woocommerce_klarna_payments_settings' );
-		$this->testmode    = $this->kp_settings['testmode'];
+		$this->testmode    = ( 'yes' !== $this->kp_settings['testmode'] ) ? false : true;
 		$this->user_agent  = apply_filters( 'http_headers_useragent', 'WordPress/' . get_bloginfo( 'version' ) . '; ' . get_bloginfo( 'url' ) ) . ' - KP:' . WC_KLARNA_PAYMENTS_VERSION . ' - PHP Version: ' . phpversion() . ' - Krokedil';
 		$order_lines_class = new KP_Order_Lines( kp_get_klarna_country() );
 		$this->order_lines = $order_lines_class->order_lines( $this->order_id );
