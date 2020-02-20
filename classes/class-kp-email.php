c6afe3c13df117a21d405472ee9faa1a16654dee
MichaelBengtsson <michael@krokedil.se>
2020-02-20 11:39:06 +0100

    Updates the text sent in the email.

--- classes/class-kp-email.php
+++ classes/class-kp-email.php
@@ -33,9 +33,29 @@ if ( ! class_exists( 'KP_Email' ) ) {
 			$settings     = get_option( 'woocommerce_klarna_payments_settings' );
 			$add_to_email = 'yes' === $settings['add_to_email'] ? true : false;
 			if ( 'klarna_payments' === $gateway_used && $add_to_email ) {
+				$klarna_cs_url  = '<a href="https://www.klarna.com/customer-service">' . esc_html__( 'Klarna', 'klarna-payments-for-woocommerce' ) . '</a>';
+				$klarna_app_url = '<a href="https://app.klarna.com/">' . esc_html__( 'Klarna App', 'klarna-payments-for-woocommerce' ) . '</a>';
 				?>
-				<p><a href="https://app.klarna.com/"><?php echo esc_html__( 'Klarna App', 'klarna-payments-for-woocommerce' ); ?></a></p>
-				<p><a href="https://www.klarna.com/customer-service"><?php echo esc_html__( 'Klarna Customer Service', 'klarna-payments-for-woocommerce' ); ?></a></p>
+				<p><?php echo esc_html__( 'Klarna order id:', 'klarna-payments-for-woocommerce' ) . ' ' . esc_html( $order->get_transaction_id() ); ?></p>
+				<p>
+					<?php
+					echo wp_kses(
+						sprintf(
+							// translators: 1. Klarna customer service URL. 2. Klarnas app url.
+							__(
+								'The payment for your purchase is handled by %1$s. 
+							If you chose to pay directly then your payment is already completed. 
+							If you chose to use either invoice or part payment then you will shortly receive more information from Klarna regarding your payment. 
+							Remember that you can manage all your payments from Klarnas website or in the %2$s',
+								'klarna-payments-for-woocommerce'
+							),
+							$klarna_cs_url,
+							$klarna_app_url
+						),
+						array( 'a' => array( 'href' => array() ) )
+					);
+					?>
+				</p>
 				<?php
 			}
 		}
