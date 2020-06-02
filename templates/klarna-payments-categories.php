baf19edb79831d687500f6ae458010a8fc7eacef
MichaelBengtsson <michael@krokedil.se>
2020-06-02 08:10:19 +0200

    Remove backup icons for payment methods.

--- templates/klarna-payments-categories.php
+++ templates/klarna-payments-categories.php
@@ -18,20 +18,6 @@ if ( is_array( WC()->session->get( 'klarna_payments_categories' ) ) ) {
 		$kp->id                = $payment_category_id;
 		$kp->title             = $payment_category_name;
 		$kp->icon              = $payment_category_icon;
-		$headers               = get_headers( $kp->icon );
-		if ( 'HTTP/1.1 404 Not Found' === $headers[0] ) {
-			switch ( $kp->id ) {
-				case 'klarna_payments_pay_later':
-					$kp->icon = 'https://cdn.klarna.com/1.0/shared/image/generic/badge/sv_se/pay_later/standard/pink.svg';
-					break;
-				case 'klarna_payments_pay_over_time':
-					$kp->icon = 'https://cdn.klarna.com/1.0/shared/image/generic/badge/sv_se/slice_it/standard/pink.svg';
-					break;
-				case 'klarna_payments_pay_now':
-					$kp->icon = 'https://cdn.klarna.com/1.0/shared/image/generic/badge/sv_se/pay_now/standard/pink.svg';
-					break;
-			}
-		}
 		wc_get_template( 'checkout/payment-method.php', array( 'gateway' => $kp ) );
 	}
 }
