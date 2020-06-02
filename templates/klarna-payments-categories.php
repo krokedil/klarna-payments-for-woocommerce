d6ef86b49c43bd8c7cb3dda71caafa2745cd5b2e
MichaelBengtsson <michael@krokedil.se>
2020-06-02 08:56:44 +0200

    Force payment category to array

--- templates/klarna-payments-categories.php
+++ templates/klarna-payments-categories.php
@@ -11,15 +11,16 @@ if ( is_array( WC()->session->get( 'klarna_payments_categories' ) ) ) {
 	$kp                 = $available_gateways['klarna_payments'];
 
 	foreach ( apply_filters( 'wc_klarna_payments_available_payment_categories', WC()->session->get( 'klarna_payments_categories' ) ) as $payment_category ) {
-		if ( is_array( $payment_category ) ) {
-			$payment_category_id   = 'klarna_payments_' . $payment_category['identifier'];
-			$payment_category_name = $payment_category['name'];
-			$payment_category_icon = $payment_category['asset_urls']['standard'];
-			$kp                    = $available_gateways['klarna_payments'];
-			$kp->id                = $payment_category_id;
-			$kp->title             = $payment_category_name;
-			$kp->icon              = $payment_category_icon;
-			wc_get_template( 'checkout/payment-method.php', array( 'gateway' => $kp ) );
+		if ( ! is_array( $payment_category ) ) {
+			$payment_category = json_decode( wp_json_encode( $payment_category ), true );
 		}
+		$payment_category_id   = 'klarna_payments_' . $payment_category['identifier'];
+		$payment_category_name = $payment_category['name'];
+		$payment_category_icon = $payment_category['asset_urls']['standard'];
+		$kp                    = $available_gateways['klarna_payments'];
+		$kp->id                = $payment_category_id;
+		$kp->title             = $payment_category_name;
+		$kp->icon              = $payment_category_icon;
+		wc_get_template( 'checkout/payment-method.php', array( 'gateway' => $kp ) );
 	}
 }
