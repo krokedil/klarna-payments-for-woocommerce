6e90c91e126ab0689c517ffcb36226c127ce71d9
Kontrazen <krlovic@gmail.com>
2021-11-29 17:30:44 +0100

    Add selected countries to system status log

--- includes/admin/views/status-report.php
+++ includes/admin/views/status-report.php
@@ -67,3 +67,66 @@ if ( ! defined( 'ABSPATH' ) ) {
 	?>
 		</tbody>
 	</table>
+
+	<?php
+		$list_of_countries      = array();
+		$test_list_of_countries = array();
+
+	foreach ( get_option( 'woocommerce_klarna_payments_settings', array() ) as $key => $value ) {
+		if ( $value != '' ) {
+			// erll( 'Settings key with value', $key . ' -> ' . $value );
+
+			if ( preg_match( '/test_merchant_id/i', $key ) ) {
+				erll( 'Key: ', $key );
+					array_push( $list_of_countries, ( strtoupper( substr( $key, -2 ) ) ) );
+			} elseif ( preg_match( '/merchant_id/i', $key ) ) {
+				erll( 'Key: ', $key );
+					array_push( $test_list_of_countries, ( strtoupper( substr( $key, -2 ) ) ) );
+			}
+		}
+	}
+	?>
+
+
+	<table class="wc_status_table widefat" autofocus>
+
+			<thead>
+				<tr>
+					<th colspan="6" data-export-label="Klarna Countries Request Log">
+						<h2><?php esc_html_e( 'Klarna Countries', 'klarna-payments-for-woocommerce' ); ?><?php echo wc_help_tip( esc_html__( 'Klarna Countries System Status.', 'klarna-payments-for-woocommerce' ) ); /* phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped */ ?></h2>
+					</th>
+				</tr>
+			</thead>
+
+
+		<tbody id="klarna-payments-countries-body">
+				<tr id="klarna-payments-countries-row-1">
+					<td ><strong><?php esc_html_e( 'Selected countries', 'klarna-payments-for-woocommerce' ); ?></strong></td>
+					<td ><strong><?php esc_html_e( 'Selected Test countries', 'klarna-payments-for-woocommerce' ); ?></strong></td>
+				</tr>
+
+
+	
+				<tr id="klarna-payments-countries-row-2">
+					<td>
+						<?php
+						if ( ( isset( $list_of_countries ) ) && ( count( $list_of_countries ) > 0 ) ) {
+							esc_html_e( implode( ',', $list_of_countries ) );
+						} else {
+							esc_html_e( 'No countries selected' );
+						}
+						?>
+					</td>
+					<td>
+						<?php
+						if ( ( isset( $test_list_of_countries ) ) && ( count( $test_list_of_countries ) > 0 ) ) {
+							esc_html_e( implode( ',', $list_of_countries ) );
+						} else {
+							esc_html_e( 'No countries selected' );
+						}
+						?>
+					</td>
+				</tr>
+
+		</tbody>
+	</table>
