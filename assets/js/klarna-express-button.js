aa7bef506e6ca14c434398ee9d8414bd82881418
Montazar Al-Jaber <montazar@duck.com>
2022-05-19 13:44:54 +0200

    Re-paint Express button element on reload

--- assets/js/klarna-express-button.js
+++ assets/js/klarna-express-button.js
@@ -7,6 +7,8 @@ jQuery(function ($) {
 	}
 
 	window.klarnaExpressButtonAsyncCallback = function () {
+		Klarna.ExpressButton.refreshButtons();
+
 		Klarna.ExpressButton.on('user-authenticated', function (callbackData) {
 			console.log('Klarna Express Button', callbackData);
 			$.ajax({
@@ -18,13 +20,13 @@ jQuery(function ($) {
 				dataType: 'JSON',
 				url: klarna_payments_express_button_params.express_button_url,
 				success: function (response) {
-					console.log('[SUCCESS] Express Button.')
+					console.log('[SUCCESS] Express Button.');
 					/* The data is the URL for the checkout page. */
 					window.location.href = response['data'];
 				},
 				error: function (response) {
 					/* The data is an error message. */
-					console.warn('[ERROR] Express Button: ', response['data'])
+					console.warn('[ERROR] Express Button: ', response['data']);
 				}
 			});
 		});
