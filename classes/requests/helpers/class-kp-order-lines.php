396ff9f0c7ad240dea2edb15f4e2cfcffa91096f
MichaelBengtsson <michael@krokedil.se>
2022-10-26 08:17:57 +0200

    Update abstract order lines helper with abstract methods

--- classes/requests/helpers/class-kp-order-lines.php
+++ classes/requests/helpers/class-kp-order-lines.php
@@ -22,4 +22,35 @@ abstract class KP_Order_Lines {
 	public static function format_number( $number ) {
 		return intval( wc_format_decimal( $number, '' ) * 100 );
 	}
+
+	/**
+	 * Filters array values to remove all null values.
+	 *
+	 * @param mixed $var The line value to filter.
+	 * @return bool
+	 */
+	public static function remove_null( $var ) {
+		return is_array( $var ) ? ! empty( $var ) : null !== $var; // If empty array, or if value is null return true to remove value.
+	}
+
+	/**
+	 * Returns processed order lines from either a WooCommerce order or cart. Has to be overriden in child class.
+	 *
+	 * @return array
+	 */
+	abstract public static function get_kp_order_lines();
+
+	/**
+	 * Returns the order amount for Klarna Payments. Has to be overriden in child class.
+	 *
+	 * @return int
+	 */
+	abstract public static function get_kp_order_amount();
+
+	/**
+	 * Returns the order tax amount for Klarna Payments. Has to be overriden in child class.
+	 *
+	 * @return int
+	 */
+	abstract public static function get_kp_order_tax_amount();
 }
