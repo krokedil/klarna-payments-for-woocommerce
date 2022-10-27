7ec3a8628fcf21364d88ca23a6b459f7de788def
MichaelBengtsson <michael@krokedil.se>
2022-10-27 09:07:50 +0200

    Fix error message for assets_urls

--- templates/klarna-payments-categories.php
+++ templates/klarna-payments-categories.php
@@ -17,7 +17,7 @@ if ( is_array( $payment_categories ) ) {
 		}
 		$payment_category_id   = 'klarna_payments_' . $payment_category['identifier'];
 		$payment_category_name = $payment_category['name'];
-		$payment_category_icon = $payment_category['assets_url'];
+		$payment_category_icon = $payment_category['assets_urls']['standard'] ?? null;
 		$kp                    = $available_gateways['klarna_payments'];
 		$kp->id                = $payment_category_id;
 		$kp->title             = $payment_category_name;
