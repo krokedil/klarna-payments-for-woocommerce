6cb6637fa30dec46cac51d97991a321c309171d0
MichaelBengtsson <michael@krokedil.se>
2022-10-27 09:21:31 +0200

    Update how we log ids with log entries to make tracking orders easier.

--- classes/requests/class-kp-requests.php
+++ classes/requests/class-kp-requests.php
@@ -263,11 +263,12 @@ abstract class KP_Requests {
 	 * @return void
 	 */
 	protected function log_response( $response, $request_args, $request_url ) {
-		$method   = $this->method;
-		$title    = "{$this->log_title} - URL: {$request_url}";
-		$code     = wp_remote_retrieve_response_code( $response );
-		$order_id = $response['session_id'] ?? $response['order_id'] ?? $this->arguments['session_id'] ?? null; // Try to get session id or order id from the response, else null.
-		$log      = KP_Logger::format_log( $order_id, $method, $title, $request_args, $response, $code, $request_url );
+		$method    = $this->method;
+		$title     = "{$this->log_title} - URL: {$request_url}";
+		$code      = wp_remote_retrieve_response_code( $response );
+		$order_id  = $response['session_id'] ?? $this->arguments['session_id'] ?? null; // Try to get session id from the response, or from the arguments.
+		$order_id .= isset( $response['order_id'] ) ? ' - ' . $response['order_id'] : ''; // Add any order id to the end, since KP returns a differnet id for the order in the end.
+		$log       = KP_Logger::format_log( $order_id, $method, $title, $request_args, $response, $code, $request_url );
 		KP_Logger::log( $log );
 	}
 }
