7a43d37592ef0522fdd25c90e3f6219eef0a6d81
MichaelBengtsson <michael@krokedil.se>
2023-01-26 13:22:35 +0100

    Update setup and teardown files to use the new helper package

--- tests/e2e/global-teardown.ts
+++ tests/e2e/global-teardown.ts
@@ -1,9 +1,17 @@
+import { GetWcApiClient, Teardown } from '@krokedil/wc-test-helper';
 import { FullConfig } from '@playwright/test';
-import { Teardown } from './utils/Setup';
+
+const {
+	BASE_URL,
+	CONSUMER_KEY,
+	CONSUMER_SECRET,
+} = process.env;
 
 const globalTeardown = async (config: FullConfig) => {
+	const wcApiClient = await GetWcApiClient(BASE_URL ?? 'http://localhost:8080', CONSUMER_KEY ?? 'admin', CONSUMER_SECRET ?? 'password');
+
 	// Destroy the test data using the WC API.
-	await Teardown();
+	await Teardown(wcApiClient);
 }
 
 export default globalTeardown;
