d0ce8a7aca43e3c98932fda149572ea7a5691797
Montazar <26507935+mntzrr@users.noreply.github.com>
2023-11-29 17:54:05 +0100

    Retrieve order key from WC order

--- classes/class-kp-assets.php
+++ classes/class-kp-assets.php
@@ -71,7 +71,12 @@ class KP_Assets {
 	private function get_checkout_params( $settings ) {
 		// Set needed variables for the order pay page handling.
 		$pay_for_order = kp_is_order_pay_page();
-		$order_id      = $pay_for_order ? absint( get_query_var( 'order-pay' ) ) : null;
+		$order_id      = $pay_for_order ? absint( get_query_var( 'order-pay', 0 ) ) : null;
+		$order_key     = null;
+		if ( ! empty( $order_id ) ) {
+			$order     = wc_get_order( $order_id );
+			$order_key = $order->get_order_key();
+		}
 
 		$customer_type = $settings['customer_type'] ?? 'b2c';
 		$order_data    = new KP_Order_Data( $customer_type, $order_id );
@@ -98,6 +103,7 @@ class KP_Assets {
 			'order_pay_page'         => $pay_for_order,
 			'pay_for_order'          => $pay_for_order,
 			'order_id'               => $order_id,
+			'order_key'              => $order_key,
 			'addresses'              => $pay_for_order ? array(
 				'billing'  => $customer['billing'],
 				'shipping' => $customer['shipping'],
