588afe7ebd96f1a6cfed1c397bcdd49e94803c4c
MichaelBengtsson <michael@krokedil.se>
2024-08-19 15:53:13 +0200

    Extend the custom scoper exclusions

--- scoper.custom.php
+++ scoper.custom.php
@@ -3,6 +3,14 @@
 function customize_php_scoper_config( array $config ): array {
     // Ignore the abspath constant when scoping.
 	$config['exclude-constants'][] = 'ABSPATH';
+	$config['exclude-constants'][] = 'KOSM_VERSION';
+	$config['exclude-classes'][] = 'WooCommerce';
+	$config['exclude-classes'][] = 'Klarna_OnSite_Messaging';
+	$config['exclude-classes'][] = 'Klarna_OnSite_Messaging_For_WooCommerce';
+	$config['exclude-classes'][] = 'WC_Product';
+	$config['exclude-functions'][] = 'kp_get_client_id';
+	$config['exclude-functions'][] = 'KP_WC';
+
 
 	return $config;
 }
