bdff582ba9b88818949707036ec8faef0a159c57
anyakro <anya@krokedil.se>
2025-02-13 11:09:04 +0100

    Tweak check

--- includes/kp-functions.php
+++ includes/kp-functions.php
@@ -272,7 +272,7 @@ function kp_is_order_pay_page() {
  * @return bool
  */
 function kp_is_wc_blocks_order( $order ) {
-	return $order && method_exists( $order, 'is_created_via' ) && $order->is_created_via( 'store-api' );
+	return $order && is_a( $order, WC_Order::class ) && $order->is_created_via( 'store-api' );
 }
 
 /**
