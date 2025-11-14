066b31c089d9e119285e64777667f191fd6ebd34
MichaelBengtsson <michael@krokedil.se>
2025-11-14 16:23:36 +0100

    Add support for setting what markets a feature is available for

--- src/PluginFeatures.php
+++ src/PluginFeatures.php
@@ -11,16 +11,16 @@ class PluginFeatures {
 	 * @var array
 	 */
 	protected $default_features = [
-		Features::PAYMENTS                    => [ "availability" => true, "markets" => [] ],
-		Features::OSM_PRODUCT_PAGE            => [ "availability" => true, "markets" => [] ],
-		Features::OSM_CART_PAGE               => [ "availability" => true, "markets" => [] ],
-		Features::OSM_PROMOTIONAL_BANNER      => [ "availability" => true, "markets" => [] ],
-		Features::KEC_ONE_STEP                => [ "availability" => true, "markets" => [] ],
-		Features::KEC_TWO_STEP                => [ "availability" => true, "markets" => [] ],
-		Features::SIWK_ACCOUNT_CREATION_PAGE  => [ "availability" => true, "markets" => [] ],
-		Features::SIWK_AUTHENTICATION_PAGE    => [ "availability" => true, "markets" => [] ],
-		Features::SIWK_CART_PAGE              => [ "availability" => true, "markets" => [] ],
-		Features::SUPPLEMENTARY_PURCHASE_DATA => [ "availability" => true, "markets" => [] ],
+		Features::PAYMENTS                    => [ "availability" => true, "markets" => [], "available_for" => [] ],
+		Features::OSM_PRODUCT_PAGE            => [ "availability" => true, "markets" => [], "available_for" => [] ],
+		Features::OSM_CART_PAGE               => [ "availability" => true, "markets" => [], "available_for" => [] ],
+		Features::OSM_PROMOTIONAL_BANNER      => [ "availability" => true, "markets" => [], "available_for" => [] ],
+		Features::KEC_ONE_STEP                => [ "availability" => true, "markets" => [], "available_for" => [] ],
+		Features::KEC_TWO_STEP                => [ "availability" => true, "markets" => [], "available_for" => [] ],
+		Features::SIWK_ACCOUNT_CREATION_PAGE  => [ "availability" => true, "markets" => [], "available_for" => [] ],
+		Features::SIWK_AUTHENTICATION_PAGE    => [ "availability" => true, "markets" => [], "available_for" => [] ],
+		Features::SIWK_CART_PAGE              => [ "availability" => true, "markets" => [], "available_for" => [] ],
+		Features::SUPPLEMENTARY_PURCHASE_DATA => [ "availability" => true, "markets" => [], "available_for" => [] ],
 	];
 
 	/**
@@ -46,6 +46,11 @@ class PluginFeatures {
 	 * @return void
 	 */
 	public function init_features() {
+		// Ensure we only initialize once per request.
+		if ( did_action( 'kp_plugin_features_initialized' ) ) {
+			return;
+		}
+
 		// Maybe migrate the legacy unavailable features option.
 		$this->maybe_migrate_legacy_unavailable_features();
 
@@ -54,21 +59,47 @@ class PluginFeatures {
 
 		// Initialize the features.
 		$this->features = $features;
+
+		do_action( 'kp_plugin_features_initialized', $this->features );
+	}
+
+	/**
+	 * Get the features and their availability.
+	 *
+	 * @return array The features and their availability.
+	 */
+	public function get_features() {
+		return $this->features;
 	}
 
 	/**
 	 * Process the response from the plugins api request, and store the available and unavailable features.
 	 *
 	 * @param array $response The response from the plugins API request.
+	 * @param array $credentials The credentials used for the request.
 	 * @param array $features The current features array to update. Passed by reference.
 	 *
 	 * @return void
 	 */
-	public function process_api_response( $response, &$features ) {
+	public function process_api_response( $response, $credentials, &$features ) {
 		foreach( $response['features'] ?? [] as $feature ) {
-			$key          = str_replace( 'platform-plugin-', '', $feature['feature_key'] ?? '' );
-			$availability = $feature['availability'] ?? 'AVAILABLE'; // Default to AVAILABLE if not set
-			$markets      = $feature['markets'] ?? [];
+			$key           = str_replace( 'platform-plugin-', '', $feature['feature_key'] ?? '' );
+			$availability  = $feature['availability'] ?? 'AVAILABLE'; // Default to AVAILABLE if not set
+			$markets       = $feature['markets'] ?? [];
+			$available_for = $features[ $key ]['available_for'] ?? []; // Get the existing available_for list from the saved features.
+
+			// Add or remove the country code from the available_for list based on availability.
+			if ( $availability === 'AVAILABLE' ) {
+				// If it is available, add the country code if not already present.
+				if ( ! in_array( $credentials['country_code'] ?? 'unknown', $available_for ) ) {
+					$available_for[] = $credentials['country_code'] ?? 'unknown';
+				}
+			} else {
+				// If it is unavailable, remove the country code if present.
+				if ( in_array( $credentials['country_code'] ?? 'unknown', $available_for ) ) {
+					$available_for = array_diff( $available_for, [ $credentials['country_code'] ?? 'unknown' ] );
+				}
+			}
 
 			// If the feature exists already, and was available before, we should not override it to unavailable.
 			if ( isset( $features[ $key ] ) && $features[ $key ]['availability'] ) {
@@ -80,7 +111,9 @@ class PluginFeatures {
 				$features[ $key ]['markets'] = array_unique ( array_merge ( $features[ $key ]['markets'] ?? [], $markets));
 			}
 
-			$features[ $key ]['availability'] = $availability === 'AVAILABLE';
+			// Update the feature availability and available_for list.
+			$features[ $key ]['availability']  = $availability === 'AVAILABLE';
+			$features[ $key ]['available_for'] = $available_for;
 		}
 	}
 
@@ -95,15 +128,14 @@ class PluginFeatures {
 			$features        = [];
 			$api_credentials = $this->get_api_credentials();
 
-			foreach ( $api_credentials as $credential ) {
-				$response = KP_WC()->api->get_unavailable_features( $credential );
+			foreach ( $api_credentials as $credentials ) {
+				$response = KP_WC()->api->get_unavailable_features( $credentials );
 
 				// If we get an error, throw an error and reset the features to default.
 				if ( is_wp_error( $response ) ) {
 					throw new \WP_Exception( __( 'There was an error when trying to get the feature availability from Klarna. Please check your API credentials and try again. Error: ' . $response->get_error_message(), 'klarna-payments-for-woocommerce' ) );
 				}
-
-				$this->process_api_response( $response, $features );
+				$this->process_api_response( $response, $credentials, $features );
 			}
 		} catch ( \WP_Exception $e ) {
 			// If we get an error, reset the features to default and log the error.
@@ -118,33 +150,115 @@ class PluginFeatures {
 	}
 
 	/**
+	 * Test a specific set of credentials and get the result of the feature availability.
+	 *
+	 * @param array $credentials The API credentials to test.
+	 *
+	 * @return array
+	 */
+	public function process_api_credentials( $credentials ) {
+		try{
+			// Use the stored features as a base since we might have other features that are available from other credentials.
+			$features = $this->features;
+			$response = KP_WC()->api->get_unavailable_features( $credentials );
+
+			// If we get an error, throw an error and reset the features to default.
+			if ( is_wp_error( $response ) ) {
+				throw new \WP_Exception( __( 'There was an error when trying to get the feature availability from Klarna. Please check your API credentials and try again. Error: ' . $response->get_error_message(), 'klarna-payments-for-woocommerce' ) );
+			}
+
+			$this->process_api_response( $response, $credentials, $features );
+		} catch ( \WP_Exception $e ) {
+			// Log the error.
+			KP_WC()->logger()->error( 'Error when trying to get the feature availability from Klarna: ' . $e->getMessage() );
+		} finally {
+			// Return the features as the result.
+			return $features;
+		}
+	}
+
+	/**
+	 * Get the sections for the settings page that should be hidden based on unavailable features from the availability.
+	 *
+	 * @param array $features The features array. From either the settings or from process_api_credentials.
+	 *
+	 * @return string[] The list of sections to hide.
+	 */
+	public static function get_sections_to_hide( $features ) {
+		$sections_to_hide = [];
+		foreach ( $features as $feature_key => $feature_data ) {
+			$new_hidden_sections = [];
+
+			switch( $feature_key ) {
+				case Features::PAYMENTS:
+				case Features::PAYMENTS_RECURRING:
+					$new_hidden_sections[] = 'general';
+					$new_hidden_sections[] = 'kom';
+					break;
+				case Features::OSM_PRODUCT_PAGE:
+				case Features::OSM_CART_PAGE:
+				case Features::OSM_PROMOTIONAL_BANNER:
+					$new_hidden_sections[] = 'onsite_messaging';
+					break;
+				case Features::KEC_ONE_STEP:
+				case Features::KEC_TWO_STEP:
+					$new_hidden_sections[] = 'kec_settings';
+					break;
+				case Features::SIWK_ACCOUNT_CREATION_PAGE:
+				case Features::SIWK_AUTHENTICATION_PAGE:
+				case Features::SIWK_CART_PAGE:
+					$new_hidden_sections[] = 'siwk';
+					break;
+			}
+
+			// If the feature is available, ensure it is not added to the unavailable sections.
+			if ( $feature_data['availability'] && ! empty( array_intersect( $new_hidden_sections, $sections_to_hide ) ) ) {
+				$sections_to_hide = array_diff( $sections_to_hide, $new_hidden_sections );
+			}
+
+			// Add it to the unavailable sections if not available.
+			if ( ! $feature_data['availability'] ) {
+				$sections_to_hide = array_merge( $sections_to_hide, $new_hidden_sections );
+				$sections_to_hide = array_unique( $sections_to_hide );
+			}
+		}
+
+		return $sections_to_hide;
+	}
+
+	/**
 	 * Check if a feature is available or not.
 	 *
 	 * @param string|array $feature_key The feature(s) key to test.
+	 * @param string|null $country_code The country code to test for. Optional. If not passed we will check general availability instead.
 	 *
 	 * @return bool True if the feature(s) is available, false otherwise. If an array is passed, if any feature is available, true is returned.
 	 */
-	public static function is_available( $feature_key ) {
+	public static function is_available( $feature_key, $country_code = null ) {
 		if ( is_array( $feature_key ) ) {
 			foreach ( $feature_key as $key ) {
-				if ( KP_WC()->plugin_features()->is_feature_available( $key ) ) {
+				if ( KP_WC()->plugin_features()->is_feature_available( $key, $country_code ) ) {
 					return true;
 				}
 			}
 			return false;
 		}
 
-		return KP_WC()->plugin_features()->is_feature_available( $feature_key );
+		return KP_WC()->plugin_features()->is_feature_available( $feature_key, $country_code );
 	}
 
 	/**
 	 * Check if a feature is available or not. Private method called by the static method is_available.
 	 *
 	 * @param string $feature_key The feature key to test.
+	 * @param string|null $country_code The country code to test for. Optional. If not passed we will check general availability instead.
 	 *
 	 * @return bool True if the feature is available, false otherwise.
 	 */
-	private function is_feature_available( $feature_key ) {
+	private function is_feature_available( $feature_key, $country_code = null ) {
+		if ( $country_code ) {
+			return isset( $this->features[ $feature_key ] ) && in_array( $country_code, $this->features[ $feature_key ]['available_for'] );
+		}
 		return isset( $this->features[ $feature_key ] ) && $this->features[ $feature_key ]['availability'];
 	}
 
