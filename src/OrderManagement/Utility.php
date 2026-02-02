a52aa49fc55bc0fa2d5e1359ba4aed92701e15cf
anyakro <anya@krokedil.se>
2026-02-02 11:24:32 +0100

    Remove duplicate hpos method

--- src/OrderManagement/Utility.php
+++ src/OrderManagement/Utility.php
@@ -17,26 +17,12 @@ class Utility {
 	 * @return int|false The order ID or false.
 	 */
 	public static function get_the_ID() {
-		$hpos_enabled = self::is_hpos_enabled();
+		$hpos_enabled = kp_is_hpos_enabled();
 		$order_id     = $hpos_enabled ? filter_input( INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT ) : get_the_ID();
 		if ( empty( $order_id ) ) {
 			return false;
 		}
 
 		return $order_id;
-	}
-
-	/**
-	 * Whether HPOS is enabled.
-	 *
-	 * @return bool
-	 */
-	public static function is_hpos_enabled() {
-		// CustomOrdersTableController was introduced in WC 6.4.
-		if ( class_exists( CustomOrdersTableController::class ) ) {
-			return wc_get_container()->get( CustomOrdersTableController::class )->custom_orders_table_usage_is_enabled();
-		}
-
-		return false;
 	}
 }
