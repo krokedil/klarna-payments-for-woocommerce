e234b8d6984300ae151f55759339bc2b26981c1b
MichaelBengtsson <michael@krokedil.se>
2026-06-24 07:46:37 +0200

    Add wp-documentor and doc generation script
    Add pronamic/wp-documentor as a dev dependency and a composer script (doc:hooks) to generate hooks documentation (filters) into docs/hooks.md using docs/template.php. Includes new docs/hook.php and docs/template.php and updates composer.lock to reflect installed dev dependencies.

---
+++ docs/template.php
@@ -0,0 +1,37 @@
+<?php
+/** @var \Pronamic\WordPress\Documentor\Documentor $documentor */
+if ( ! isset( $documentor ) ) {
+	return;
+}
+
+$actions = $documentor->get_actions();
+$filters = $documentor->get_filters();
+
+$eol = "\n";
+
+echo '# Hooks', $eol;
+
+echo $eol;
+
+echo '- [Actions](#actions)', $eol;
+echo '- [Filters](#filters)', $eol;
+
+echo $eol;
+
+echo '## Actions', $eol;
+
+echo $eol;
+
+foreach ( $actions as $hook ) {
+	include __DIR__  . '/hook.php';
+}
+
+echo '## Filters', $eol;
+
+echo $eol;
+
+foreach ( $filters as $hook ) {
+	include __DIR__  . '/hook.php';
+}
+
+echo $eol;
