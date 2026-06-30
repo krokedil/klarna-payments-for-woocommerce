1c0cb02b915420985a9dcfa974049b85758bbf52
anyakro <anya@krokedil.se>
2026-06-30 09:46:18 +0200

    Fix textdomain loaded warning (remove unused code)

--- src/SignInWithKlarna/Settings.php
+++ src/SignInWithKlarna/Settings.php
@@ -353,11 +353,10 @@ class Settings {
 	 */
 	private function default() {
 		return array(
-			'siwk_client_id'      => '',
-			'siwk_enabled'        => 'no',
-			'siwk_test_mode'      => 'no',
-			'siwk_title_theme'    => __( 'Theme, button shape & placements', 'klarna-payments-for-woocommerce' ),
-			'siwk_button_theme'   => 'default',
+			'siwk_client_id'    => '',
+			'siwk_enabled'      => 'no',
+			'siwk_test_mode'    => 'no',
+			'siwk_button_theme' => 'default',
 			'siwk_button_shape'   => 'default',
 			'siwk_logo_alignment' => 'default',
 			'siwk_cart_placement' => 10,
